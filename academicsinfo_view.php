086d4a10061314179a8408dbefeea861769055e6
pratikshakravate <=>
2024-07-23 17:11:40 +0530

    update changes

--- academicsinfo_view.php
+++ academicsinfo_view.php
@@ -69,7 +69,6 @@
                 <tr>
                     <td><?php echo ++$count; ?></td>
                     <td>
-                        <!-- <a href="slider_details.php?Slider_id=<?php// echo $fetch['Slider_id'] ?>"  class="fa fa-eye text-primary" style="font-size: 20px;"></a> -->
                         
                         <a href="delete_academicsinfo.php?fld_academics_id=<?php echo $fetch['fld_academics_id'] ?>" onclick="refld_admin_idturn confirm('Are you sure to Activity Information Record Deleted')" class="fa fa-trash-o"  style="color: red;font-size: 20px;"></a>
                         <a href="update_academicsinfo.php?fld_academics_id=<?php echo $fetch['fld_academics_id'] ?>"  class="fa fa-edit"  style="color: green;font-size: 20px;"></a>
