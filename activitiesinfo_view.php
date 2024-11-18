d90c79eab6313fa276428db1049a916a663b0602
pratikshakravate <=>
2024-11-18 16:44:43 +0530

    new changes

--- activitiesinfo_view.php
+++ activitiesinfo_view.php
@@ -35,7 +35,6 @@
             </div>
           </div>
         </div>
-        <!-- Export Datatable start -->
         <div class="pd-20 bg-white border-radius-4 box-shadow mb-30">
           <div class="clearfix mb-20">
             <div class="pull-left">
