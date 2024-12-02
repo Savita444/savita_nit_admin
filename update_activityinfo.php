b037ea4f1d923267e7b791f671abccd64255ebef
Savita444 <savita.hajare@sumagoinfotech.com>
2024-12-02 17:19:07 +0530

    changes

--- update_activityinfo.php
+++ update_activityinfo.php
@@ -78,8 +78,8 @@
                               while($row=mysqli_fetch_assoc($query1)){
                                 extract($row);
                             ?>
-                                    <option value="<?php echo $row['activity_id']; ?>"
-                                        <?php if($fetch['activity_id']==$row['activity_id']) {echo "selected";} ?>>
+                                    <option value="<?php echo $row['id']; ?>"
+                                        <?php if($fetch['activity_id']==$row['id']) {echo "selected";} ?>>
                                         <?php echo $row['activities']; ?></option>
                                     <?php  }?>
                                 </select>
